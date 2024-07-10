2d62d892649ce41ad68e770e6a4a7bde35ea1df4
Cypher Moon <temperaturecaleb@gmail.com>
2024-07-10 14:04:19 +0100

    Add interface to interact with the logic

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+const primeNumberForm = document.querySelector("form.prime_number_form")
+const primeInputNumber = document.querySelector("#prime_number_input")
+const primeNumberOutput = document.querySelector("#prime_number_output")
+
 const testPrimeNumber = 2
 
+
+
+/*** UTILITY FUNCTIONS *******/
 function convertToNumber(_input) {
     const input = parseInt(_input)
 
@@ -28,7 +35,6 @@ function isPrimeNumber(_inputNumber) {
 }
 
 
-
 function getPrimeNumberBetweenRange(_lowerRange, _upperRange) {
     const lowerRange = convertToNumber(_lowerRange)
     const upperRange = convertToNumber(_upperRange)
@@ -41,3 +47,65 @@ function getPrimeNumberBetweenRange(_lowerRange, _upperRange) {
 
     return primeNumbers
 }
+
+function resetValues() {
+    primeInputNumber.value = ""
+    primeNumberOutput.textContent = "Please submit a number to check 'primeness' of the number"
+
+}
+
+function convertInputToRangeFormat(inputNumber) {
+    // check if the input is in a range format e.g 1-2
+    const rangeRegex = /(\d\-\d)/g
+
+    if (!rangeRegex.test(inputNumber)) return [];
+
+    const range = inputNumber.split("-")
+    return range
+}
+
+
+
+
+/***** User Interface Manipulation */
+
+primeNumberForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const inputNumber = primeInputNumber.value
+    const range = convertInputToRangeFormat(inputNumber)
+
+    // Check if it is a range instead of an ordinary number
+    if (range.length > 0) {
+
+        const primeNumbers = getPrimeNumberBetweenRange(range[0], range[1])
+
+        primeNumberOutput.textContent = `Prime numbers between ${range[0]} and ${range[1]} are: ${primeNumbers.join(", ")}`
+
+        setTimeout(() => {
+            resetValues()
+        }, 3000)
+        return
+    }
+
+    try {
+        // check if the input is a prime number
+        // Output 'Prime!' if it is
+        if (isPrimeNumber(inputNumber).isPrime) {
+            primeNumberOutput.textContent = `Prime!`
+        }
+        // Otherwise Print the factors
+        else {
+            primeNumberOutput.textContent = `${inputNumber} is not a prime number. Factors: ${isPrimeNumber(inputNumber).factors.join(", ")}`
+        }
+
+        // reset the values after three seconds
+        setTimeout(() => {
+            resetValues()
+        }, 3000)
+
+    } catch (err) {
+        alert(err.message)
+        resetValues()
+    }
+})
